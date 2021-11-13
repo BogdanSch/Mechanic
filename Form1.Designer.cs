4112fd965848bd0caede7f3ebee151bda5ec1ec8
Kit <Kit@KIT11-15>
2021-11-13 16:47:57 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace MechanicTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bStart = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(813, 12);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(82, 23);
+            this.bStart.TabIndex = 0;
+            this.bStart.Text = "Draw";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel.Location = new System.Drawing.Point(12, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(795, 585);
+            this.panel.TabIndex = 1;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 609);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.bStart);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Mechanic";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using Mechanics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VectorLib;
+
+namespace MechanicTest
+{
+    public partial class Form1 : Form
+    {
+        double scale = 100;
+        List<Ball> balls = new List<Ball>();
+        Graphics g;
+        Coords[] edges;
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel.CreateGraphics();
+            edges = new Coords[]{ 
+                new Coords(0, 0),
+                new Coords(panel.Width  / scale, 0),
+                new Coords(panel.Width / scale, panel.Height / scale),
+                new Coords(0, panel.Height / scale),
+            };
+        }
+
+        public void UpdateBalls()
+        {
+            g.Clear(panel.BackColor);
+            foreach (Ball b in balls)
+            {
+                b.Move(timer.Interval / 1000.0, new Vector(0, 0));
+                Collisions.Collide(b, edges);
+                foreach (var b2 in balls)
+                {
+                    if(b == b2) continue;
+
+                    //if (Collisions.IsColliding(b, b2))
+                    //{
+                    //    b.V.Paint(g, Pens.Pink, b.R.GetPoint());
+                    //    b2.V.Paint(g, Pens.LightGreen, b.R.GetPoint());
+                    
+                    Collisions.Collide(b, b2);
+
+                        //b.V.Paint(g, Pens.Red, b.R.GetPoint());
+                        //b2.V.Paint(g, Pens.Green, b.R.GetPoint());
+                        //Update();
+
+                    //}
+                }
+
+                g.DrawEllipse(
+                    Pens.Red,
+                    (int)((b.R.X - b.Radius) * scale),
+                    (int)((b.R.Y - b.Radius) * scale),
+                    (int)(b.Radius * 2 * scale),
+                    (int)( b.Radius * 2 * scale)
+                );
+            }
+            Update();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            balls.Add(new Ball()
+            {
+                M = 5,
+                V = new Vector(4, -3),
+                Radius = 0.5,
+                R = new Vector(1, 5),
+            });
+
+            balls.Add(new Ball()
+            {
+                M = 3,
+                V = new Vector(-2, -5),
+                Radius = 0.3,
+                R = new Vector(5, 5)
+            });
+
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            UpdateBalls();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keys.Space == e.KeyCode)
+            {
+                bStart_Click(bStart, new EventArgs());
+            }
+        }
+    }
+}
